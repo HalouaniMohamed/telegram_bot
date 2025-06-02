971af951b1885512f5cc235f59226af872e0ef38
Abir-Kharmachi <99890334+Abir-Kharmachi@users.noreply.github.com>
2025-06-03 01:21:08 +0200

    a sécurité

---
+++ src/main/java/fr/ensim/interop/introrest/config/WebConfig.java
@@ -0,0 +1,19 @@
+package fr.ensim.interop.introrest.config;
+
+import fr.ensim.interop.introrest.security.ApiTokenInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private ApiTokenInterceptor apiTokenInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(apiTokenInterceptor)
+                .addPathPatterns("/joke/**"); // protège toutes les routes /joke/*
+    }
+}
